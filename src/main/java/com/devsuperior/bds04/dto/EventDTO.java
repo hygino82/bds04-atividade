58a6fcf9e68c395e7c93703864f2a1da3f497ee2
Adroaldo <hygino@msn.com>
2022-06-03 12:51:24 -0300

    Criadas as entidades User, Role e adicionando ao seed

--- src/main/java/com/devsuperior/bds04/dto/EventDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/EventDTO.java
@@ -3,15 +3,25 @@ package com.devsuperior.bds04.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import com.devsuperior.bds04.entities.Event;
+
 
 public class EventDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank
 	private String name;
+	
+	
 	private LocalDate date;
 	private String url;
+	
+	@NotNull
 	private Long cityId;
 	
 	public EventDTO() {
