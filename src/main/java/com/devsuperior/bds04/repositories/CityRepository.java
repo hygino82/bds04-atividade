aa0a9a8f579c8de6573b5c297f65e8aea249aed6
Adroaldo <hygino@msn.com>
2022-06-03 13:47:08 -0300

    Implementado controller e service das entidades

--- src/main/java/com/devsuperior/bds04/repositories/CityRepository.java
+++ src/main/java/com/devsuperior/bds04/repositories/CityRepository.java
@@ -2,8 +2,8 @@ package com.devsuperior.bds04.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.entities.City;
 
-public interface CityRepository extends JpaRepository<Event, Long>{
+public interface CityRepository extends JpaRepository<City, Long>{
 
 }
