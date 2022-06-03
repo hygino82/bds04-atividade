81e1e63b9b83a7dc7143e613056f642329747256
Adroaldo <hygino@msn.com>
2022-06-03 20:39:03 -0300

    Transactional findAll fix

--- src/main/java/com/devsuperior/bds04/services/CityService.java
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -18,6 +18,7 @@ public class CityService {
 	@Autowired
 	private CityRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<CityDTO> findAll() {
 		List<City> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
